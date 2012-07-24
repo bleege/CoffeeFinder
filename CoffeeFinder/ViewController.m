48b571fbcaf7ab7e69e0e276bca3ec1aa517089e
Brad Leege <bleege@gmail.com>
2012-07-23 21:43:07 -0500

    loading location pin with callout onto map based on data from google geocoding service

--- CoffeeFinder/ViewController.m
+++ CoffeeFinder/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
 @interface ViewController ()
 
@@ -36,21 +37,58 @@
 - (IBAction)handleStumptownClick:(id)sender
 {
     NSLog(@"handleStumptownClick() called...");
+    [mapView removeAnnotations:[mapView annotations]];
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSLog(@"do async loading of data");
         NSData *responseData = [NSData dataWithContentsOfURL: [NSURL URLWithString:@"http://maps.googleapis.com/maps/api/geocode/json?address=18+West+29th+St,+New+York,+NY&sensor=false"]];
 
+        // Parse the response data
         NSError *error;
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
         
-//        [self performSelectorOnMainThread:@selector(fetchedData:) withObject:data waitUntilDone:YES];
-
+        // Send results back to UI for display
         dispatch_async(dispatch_get_main_queue(), ^{
             NSLog(@"Back to the main thread...");
-            NSLog(@"Status = '%@'", [json objectForKey:@"status"]);
+
+            if (error != nil)
+            {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [alert show];
+            }
+            else
+            {
+                NSString *status = [json objectForKey:@"status"];
+                NSLog(@"Status = '%@'", status);
+
+                if ([[status uppercaseString] isEqualToString:@"OK"])
+                {
+                    NSLog(@"good to go...");
+                    NSArray *results = [json objectForKey:@"results"];
+                    NSDictionary *root = [results objectAtIndex:0];
+                    NSDictionary *geometry = [root objectForKey:@"geometry"];
+                    NSDictionary *loc = [geometry objectForKey:@"location"];
+                    NSDecimalNumber *lat = [loc objectForKey:@"lat"];
+                    NSDecimalNumber *lon = [loc objectForKey:@"lng"];
+                    NSLog(@"Lat = '%@', Lon = '%@'", lat, lon);
+                    
+                    MKPointAnnotation *pt = [[MKPointAnnotation alloc] init];
+                    CLLocationCoordinate2D coord = CLLocationCoordinate2DMake([lat doubleValue], [lon doubleValue]);
+                    [pt setCoordinate:coord];
+                    [pt setTitle:@"Stumptown Coffee"];
+                    [pt setSubtitle:[root objectForKey:@"formatted_address"]];
+
+                    [mapView addAnnotation:pt];
+                    MKCoordinateSpan span = MKCoordinateSpanMake(0.002401f, 0.003433f);
+                    [mapView setRegion:MKCoordinateRegionMake(coord, span) animated:YES];
+                }
+                else
+                {
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Bad Status From Google" message:status delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                    [alert show];
+                }
+            }
         });
-        
     });
     NSLog(@"done with handleStumptonClick().");
 }
